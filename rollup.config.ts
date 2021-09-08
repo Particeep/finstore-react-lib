f2270ac8aaa2429266e71fa3d9e68c81787f1bb4
fhammache <faredj.hammache@particeep.com>
2021-09-08 16:18:35 +0200

    rollup config

---
+++ rollup.config.ts
@@ -0,0 +1,32 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "rollup-plugin-typescript2"
+import json from "@rollup/plugin-json";
+import copy from "rollup-plugin-copy"
+
+const packageJson = require("./package.json")
+
+export default {
+  input: "src/components/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    copy(),
+    json()
+  ]
+}
