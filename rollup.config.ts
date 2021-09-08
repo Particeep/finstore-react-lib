515422e93a42b0f375c8512c3e2dbf75495f7903
fhammache <faredj.hammache@particeep.com>
2021-09-08 17:00:03 +0200

    fix rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,8 +2,9 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import typescript from "rollup-plugin-typescript2"
-import json from "@rollup/plugin-json";
+import json from "@rollup/plugin-json"
 import copy from "rollup-plugin-copy"
+import url from "rollup-plugin-url"
 
 const packageJson = require("./package.json")
 
@@ -27,6 +28,10 @@ export default {
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     copy(),
-    json()
+    json(),
+    url({
+      include: ['**/*.woff', '**/*.woff2'],
+      limit: Infinity,
+    })
   ]
 }
