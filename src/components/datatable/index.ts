db2345ac48056447f75466c5fb8977e3c0e1a43b
fhammache <faredj.hammache@particeep.com>
2021-09-09 17:57:33 +0200

    build

--- src/components/datatable/index.ts
+++ src/components/datatable/index.ts
@@ -1,5 +1,5 @@
 export { default as Datatable } from "./Datatable"
-export { DatatableProps } from "./DatatableProps"
+export * from "./DatatableProps"
 export * from "./helpers/columnVisibility"
 export * from "./models/models"
 export * from "./helpers/defaultSearchCriteria"
