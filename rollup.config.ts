6ffc43ced624eaeef21f48092224d54355ad8f8e
fhammache <faredj.hammache@particeep.com>
2021-09-13 15:13:01 +0200

    rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -11,16 +11,8 @@ const packageJson = require("./package.json")
 export default {
   input: "src/index.ts",
   output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
+    { file: packageJson.main,   format: "cjs", sourcemap: true },
+    { file: packageJson.module, format: "esm", sourcemap: true }
   ],
   plugins: [
     peerDepsExternal(),
